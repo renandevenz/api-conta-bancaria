d926dd570c5c93564b2aade623fdf7deefd85fd9
Renan Devenz <renan.devenz@hotmail.com>
2021-05-05 09:22:10 -0300

    Initial commit

---
+++ src/main/java/com/apibanco/cadastro/ApiContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.apibanco.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiContaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apibanco/cadastro/ApiContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apibanco.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
